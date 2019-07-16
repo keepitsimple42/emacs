494debbd4a4ffe43c9bbbdcad8e1782fb5925dba
keepitsimple42 <mark.bernhardt1@gmail.com>
2019-07-16 07:23:34 +0000

    added lisp directory

---
+++ lisp/myhydras.el
@@ -0,0 +1,18 @@
+(defhydra hydra-global-org (:color blue)
+  "Org"
+  ("n" outline-next-visible-heading "next heading")
+  ("p" outline-previous-visible-heading "prev heading")
+  ("N" org-forward-heading-same-level "next heading at same level")
+  ("P" org-backward-heading-same-level "prev heading at same level")
+  ("u" outline-up-heading "up heading")
+  ("t" org-timer-start "Start Timer")
+  ("s" org-timer-stop "Stop Timer")
+  ("r" org-timer-set-timer "Set Timer") ; This one requires you be in an orgmode doc, as it sets the timer for the header
+  ("p" org-timer "Print Timer") ; output timer value to buffer
+  ("w" (org-clock-in '(4)) "Clock-In") ; used with (org-clock-persistence-insinuate) (setq org-clock-persist t)
+  ("o" org-clock-out "Clock-Out") ; you might also want (setq org-log-note-clock-out t)
+  ("j" org-clock-goto "Clock Goto") ; global visit the clocked task
+  ("c" org-capture "Capture") ; Don't forget to define the captures you want http://orgmode.org/manual/Capture.html
+  ("l" org-capture-goto-last-stored "Last Capture")
+  
+  )
